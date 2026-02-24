e377c766b8755a04e2b158e3f33fd8d87c5fef3b
Sami Ullah <samibutt8011@gmail.com>
2026-02-24 23:29:11 +0500

    fixed

--- src/render/renderTicker.ts
+++ src/render/renderTicker.ts
@@ -1,4 +1,4 @@
-import { createCanvas, loadImage } from 'canvas';
+import { createCanvas, loadImage } from '@napi-rs/canvas';
 import { Game, Team } from '../types';
 import { config } from '../config';
 import { LogoCache } from '../utils/logoCache';
